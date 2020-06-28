6d651c2a4cfe2bb9d65c3d12096b6d0ddd9c80e8
Will Ballard <wballard@mailframe.net>
2020-06-28 06:04:40 -0500

    read in a config, with home directory option

---
+++ packages/cli/src/configuration.ts
@@ -0,0 +1,53 @@
+import fs from "fs-extra";
+import expandTide from "expand-tilde";
+import path from "path";
+import { cosmiconfig } from "cosmiconfig";
+
+/**
+ * Look here to know everything that can be configured.
+ */
+export type Configuration = {
+  AWS_ACCESS_KEY_ID: string;
+  AWS_SECRET_ACCESS_KEY: string;
+};
+
+/**
+ * Load up the configuration with the 'new-normal' cosmic config thing.
+ *
+ * The one difference is -- we'll be looking for a ~/.shipula.json as well that
+ * will be a default backstop location for AWS keys.
+ */
+export const getConfig = async (): Promise<Configuration> => {
+  let initialConfig = {
+    AWS_ACCESS_KEY_ID: undefined,
+    AWS_SECRET_ACCESS_KEY: undefined,
+  };
+  try {
+    // defaults from user home dotfile
+    const userHomeConfig = path.join(expandTide("~"), ".shipula.json");
+    if (await fs.pathExists(userHomeConfig)) {
+      const userHomeConfigProps = JSON.parse(
+        await fs.readFile(userHomeConfig, "utf8")
+      );
+      // keep on writing over -- discovery new props
+      initialConfig = {
+        ...initialConfig,
+        ...userHomeConfigProps,
+      };
+    }
+    const explorer = cosmiconfig("shipula");
+    const configProps = await explorer.search();
+    // per project config is the 'most specific', so it will
+    // write over user home directory
+    // I used to say 'trumps the config' a lot, but ...
+    initialConfig = {
+      ...initialConfig,
+      ...configProps.config,
+    };
+  } catch (e) {
+    console.error(e);
+  } finally {
+    // be extremely forgiving
+    return initialConfig;
+  }
+};
--- packages/cli/src/info.ts
+++ packages/cli/src/info.ts
@@ -1,12 +1,15 @@
 import { Command } from "commander";
 import docs from "./docs";
+import { getConfig } from "./configuration";
 
 export default new Command()
-  .command("destroy [package]")
+  .command("info [package]")
   .description("This will tell you all about your App.")
   .on("--help", () => {
     console.log(docs("info.md"));
   })
   .action(async () => {
+    const config = await getConfig();
+    console.log(config);
     return;
   });
