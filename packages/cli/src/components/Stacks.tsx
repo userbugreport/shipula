6e69719e5fb4fc428ed49f6623aa24fef387a8c2
Will Ballard <wballard@mailframe.net>
2020-07-05 09:13:14 -0500

    destroy command is destroying

--- packages/cli/src/components/Stacks.tsx
+++ packages/cli/src/components/Stacks.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { CloudFormation } from "aws-sdk";
 import { Box, Text } from "ink";
-import termSize from "term-size";
+import useStdoutDimensions from "ink-use-stdout-dimensions";
 
 type Props = {
   /**
@@ -14,6 +14,7 @@ type Props = {
  * List out stacks.
  */
 export const Stacks: React.FC<Props> = ({ stacks }) => {
+  const [columns] = useStdoutDimensions();
   // re-format the table into the columns we like
   const getTag = (stack: CloudFormation.Stack, key: string): string => {
     return stack.Tags?.find((t) => t.Key === key)?.Value;
@@ -21,33 +22,36 @@ export const Stacks: React.FC<Props> = ({ stacks }) => {
 
   return (
     <>
-      <Box flexDirection="column" width={termSize().columns}>
+      <Box flexDirection="column" width={columns}>
         <Box>
-          <Box width="30%">
-            <Text>Name</Text>
-          </Box>
-          <Box width="20">
-            <Text>Status</Text>
-          </Box>
           <Box width="20%">
             <Text>Package</Text>
           </Box>
           <Box width="20%">
             <Text>Stack</Text>
           </Box>
+          <Box width="20">
+            <Text>Status</Text>
+          </Box>
+          <Box width="50%">
+            <Text>Arguments</Text>
+          </Box>
         </Box>
         {stacks.map((stack) => (
           <Box key={stack.StackId}>
-            <Box width="30%">
-              <Text>{stack.StackName}</Text>
-            </Box>
-            <Box width="20">
-              <Text>{stack.StackStatus}</Text>
-            </Box>
             <Box width="20%">
               <Text>{getTag(stack, "packageName")}</Text>
             </Box>
             <Box width="20%">
+              <Text>{getTag(stack, "stackName")}</Text>
+            </Box>
+            <Box width="20">
+              <Text>{stack.StackStatus}</Text>
+            </Box>
+            <Box width="50%" flexDirection="row">
+              <Text>--packageName </Text>
+              <Text>{getTag(stack, "packageName")} </Text>
+              <Text>--stackName </Text>
               <Text>{getTag(stack, "stackName")}</Text>
             </Box>
           </Box>
--- packages/cli/src/destroy.ts
+++ packages/cli/src/destroy.ts
@@ -1,5 +1,5 @@
 import { Command } from "commander";
-import docs from "./docs";
+import docs, { StackName, PackageName } from "./docs";
 import { Destroy } from "./components/Destroy";
 import { display } from "./components/application";
 import { buildInfoProps } from "./context";
@@ -7,6 +7,8 @@ import { buildInfoProps } from "./context";
 export default new Command()
   .command("destroy")
   .description("Completely remove a Stack from an App")
+  .option("--packageName <packageName>", PackageName)
+  .option("--stackName <stackName>", StackName)
   .on("--help", () => {
     console.log(docs("destroy.md"));
   })
