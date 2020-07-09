4b3d08c8b6b94eef9f6a0043ccc44e454c62b0ac
Will Ballard <wballard@mailframe.net>
2020-07-09 09:49:50 -0500

    cli build

--- packages/cli/bin/index.js
+++ packages/cli/bin/index.js
@@ -1,17 +1,7 @@
 #!/usr/bin/env node
 
-// bootstrap typescript from the command line without a prebuild
-require("ts-node").register({
-  /* options */
-});
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require("path");
+const { main } = require(path.join(__dirname, "..", "lib"));
 
-const { buildProgram, main } = require(path.join(
-  __dirname,
-  "..",
-  "src",
-  "index"
-));
 // load and go
 main();
