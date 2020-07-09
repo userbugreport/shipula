8b410abccfcfee315dc27f2770b75bac53d7e82e
Will Ballard <wballard@mailframe.net>
2020-07-09 12:08:21 -0500

    try typescript

--- packages/cli/bin/index.js
+++ packages/cli/bin/index.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 
+require("ts-node").register({});
 const path = require("path");
-const { main } = require(path.join(__dirname, "..", "lib"));
+const { main } = require(path.join(__dirname, "..", "src"));
 
 // load and go
 main();
