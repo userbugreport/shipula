4104a4004b2f84e9519e7dc1a8ee16b68da22cdc
Will Ballard <wballard@mailframe.net>
2020-09-28 08:55:28 -0500

    WIP #18 -- wildcard created, now to use it

--- packages/domain/src/index.ts
+++ packages/domain/src/index.ts
@@ -1,27 +1,47 @@
 import * as cdk from "@aws-cdk/core";
+import * as acm from "@aws-cdk/aws-certificatemanager";
 import * as route53 from "@aws-cdk/aws-route53";
+import * as ssm from "@aws-cdk/aws-ssm";
 
 const app = new cdk.App();
 
-// get all parameters
-const domainName = process.env.DOMAIN_NAME;
+interface ShipulaDomainStackProps extends cdk.StackProps {
+  domainName: string;
+}
 
 class ShipulaDomain extends cdk.Stack {
-  constructor(parent: cdk.App, name: string, props: cdk.StackProps) {
+  constructor(parent: cdk.App, name: string, props: ShipulaDomainStackProps) {
     super(parent, name, props);
-    const zone = new route53.PublicHostedZone(this, domainName, {
-      zoneName: domainName,
+    const zone = new route53.PublicHostedZone(this, props.domainName, {
+      zoneName: props.domainName,
       comment: "Created by Shipula",
     });
     new route53.TxtRecord(this, "shipula", {
       zone,
       values: ["Hello from Shipula!"],
     });
+    // certificate must be in us-east-1 for cloudfront
+    const wildcardCertificate = new acm.DnsValidatedCertificate(
+      this,
+      "SiteCertificate",
+      {
+        domainName: `*.${props.domainName}`,
+        hostedZone: zone,
+        region: "us-east-1",
+      }
+    );
+    // store this arn so we can look it up easily
+    new ssm.StringParameter(this, "WildcardCertificate", {
+      parameterName: `/shipula/${props.domainName}/wildcard`,
+      stringValue: wildcardCertificate.certificateArn,
+    });
   }
 }
 
 const main = async () => {
+  const domainName = process.env.DOMAIN_NAME;
   new ShipulaDomain(app, domainName.replace(".", "-"), {
+    domainName,
     env: {
       account: process.env.CDK_DEFAULT_ACCOUNT,
       region: process.env.CDK_DEFAULT_REGION,
--- packages/network/src/Network.ts
+++ packages/network/src/Network.ts
@@ -8,8 +8,23 @@ export class Network extends cdk.Stack {
 
     // this VPC will be shared across all shipula in a Region
     // this is to share IPs against the very low EIP limit
-    const vpc = new ec2.Vpc(this, "vpc", { maxAzs: 2 });
+    const vpc = new ec2.Vpc(this, "vpc", {
+      maxAzs: 2,
+      gatewayEndpoints: {
+        S3: {
+          service: ec2.GatewayVpcEndpointAwsService.S3,
+        },
+      },
+    });
     cdk.Tags.of(vpc).add(id, "true");
+    const ecr = vpc.addInterfaceEndpoint("EcrEndpoint", {
+      service: ec2.InterfaceVpcEndpointAwsService.ECR,
+    });
+    ecr.connections.allowDefaultPortFromAnyIpv4();
+    const ecr_docker = vpc.addInterfaceEndpoint("EcrDockerEndpoint", {
+      service: ec2.InterfaceVpcEndpointAwsService.ECR_DOCKER,
+    });
+    ecr_docker.connections.allowDefaultPortFromAnyIpv4();
 
     // and that is it -- tagged network will be looked up by name from
     // any shipula deploys in this Region
